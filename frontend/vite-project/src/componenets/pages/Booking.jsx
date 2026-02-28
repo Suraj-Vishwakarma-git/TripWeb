790c7298eb950e06a180b776306317f7119309cd
Suraj <svsurajshailendra@gmail.com>
2026-02-28 16:15:03 +0530

    Edited

--- frontend/vite-project/src/componenets/pages/Booking.jsx
+++ frontend/vite-project/src/componenets/pages/Booking.jsx
@@ -2,11 +2,37 @@ import React from 'react'
 import "./Booking.css";
 import { Link } from 'react-router-dom';
 import { useNavigate } from "react-router-dom";
+import { useState } from 'react';
 
 const Booking = ({binfo}) => {
   const navigate = useNavigate();
+  const countries = [
+  "India",
+  "United States",
+  "Canada",
+  "Brazil",
+  "Germany",
+  "France",
+  "United Kingdom",
+  "Australia",
+  "Japan",
+  "China",
+  "South Korea",
+  "Russia",
+  "Italy",
+  "Spain",
+  "Mexico",
+  "South Africa",
+  "Argentina",
+  "Netherlands",
+  "Switzerland",
+  "Singapore"
+];
+
+ const [selectedcountry,setselectedcountry]=useState("");
 
   async function book(){
+     e.preventDefault(); 
     const token = localStorage.getItem("token");
      const API=await fetch("http://localhost:5000/api/auth/booking",{
       method:"POST",
@@ -26,14 +52,25 @@ const Booking = ({binfo}) => {
     <div>
      <div className="box">
       <h1>Book Ticket</h1>
+      <form onSubmit={book}>
       <div className='product-card' key={binfo._id}>
         <img src={binfo.image} alt={binfo.title} id='img'/>
         <h3 className='product-title'>{binfo.title}</h3>
         <p className='product-price'>${binfo.ticketPrice}</p>
-      <button className='Playment-btn'onClick={book} >Pay Now</button> 
-        </div>
-   
       
+        <select value={selectedcountry} onChange={(e)=>setselectedcountry(e.target.value)} required>
+          <option value="">Select Country</option>
+          {
+            countries.map((e)=>(
+              <option value={e} key={e} >{e}</option>
+            ))
+          }
+        </select>
+      <button className='Playment-btn' type='submit'>Pay Now</button> 
+       
+       
+   </div>
+      </form>
       </div> 
      
     </div>
